ee2c9da9b6df7408acd4f00982ed6f00847eef9d
Sonam Kumari <sonamkumari@Mac.home>
2025-01-07 16:28:54 +0100

    add dropdown button and their logic add elevated button with error dialos and show alert dialog for user input

--- lib/widgets/new_expense.dart
+++ lib/widgets/new_expense.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 
+import 'package:expense_daily/models/expense.dart';
+
 class NewExpense extends StatefulWidget {
   const NewExpense({super.key});
-  
+
   @override
   State<NewExpense> createState() {
     return _NewExpenseState();
@@ -10,24 +12,162 @@ class NewExpense extends StatefulWidget {
 }
 
 class _NewExpenseState extends State<NewExpense> {
-  
+  //For sinfgle value
+//   var _enteredTitle = '';
+
+// void _saveTitleInput(String inputValue) {
+//   _enteredTitle = inputValue;
+// }
+
+//For Drop down Button
+  Category _selectedCategory = Category.leisure;
+
+//for Date
+  DateTime? _selectedDate;
+  void _presentDatePicker() async {
+    final now = DateTime.now();
+    final firstDate = DateTime(now.year - 1, now.month, now.day);
+    // final lastDate = DateTime.now();
+    final pickedDate = await showDatePicker(
+      context: context,
+      /*intialDate: now,*/
+      firstDate: firstDate,
+      lastDate: now,
+    ); /*.then((value) {
+    },);*/
+    //this line only executed when value picked
+    setState(() {
+      _selectedDate = pickedDate;
+    });
+  }
+
+//Create controller Text Editing
+  final _titleController = TextEditingController();
+  final _amountController = TextEditingController();
+
+//For deleted Text Editing Controller
+  @override
+  void dispose() {
+    _titleController.dispose();
+    _amountController.dispose();
+    super.dispose();
+  }
+
+  //For validation
+  void _submitExpenseData() {
+    final enteredAmount = double.tryParse(_amountController.text); // tryparse('Hello) => null, trypass('1.12') => 1.12
+    final amountIsInvalid = enteredAmount == null || enteredAmount <= 0;
+
+    if (_titleController.text.trim().isEmpty || 
+    amountIsInvalid == true ||
+     _selectedDate == null) {
+      //show error message
+      showDialog(context: context, builder: (ctx) => AlertDialog(
+          title: const Text('Invalid Input'),
+          content: const Text('Please make sure a valid title, amount, date and category was enetered'),
+          actions: [
+            TextButton(onPressed: () {
+              Navigator.pop(ctx);
+            }, 
+            child: const Text('OK'),
+            ),
+          ],
+      ),
+      );
+      return;
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.all(16),
       child: Column(
-        children: const [
+        children: [
           TextField(
+            //onChanged: _saveTitleInput,
+            controller: _titleController,
             maxLength: 50,
-            // keyboardType: TextInputType.text,
+            // keyboardType: TextInputType.number,
             decoration: InputDecoration(
               label: Text('Title'),
             ),
+          ),
+          Row(
+            children: [
+              Expanded(
+                child: TextField(
+                  //onChanged: _saveTitleInput,
+                  controller: _amountController,
+                  // maxLength: 50,
+                  keyboardType: TextInputType.number,
+                  decoration: InputDecoration(
+                    prefixText: '\$ ',
+                    label: Text('Amount'),
+                  ),
+                ),
+              ),
+              const SizedBox(
+                width: 10,
+              ),
+              Expanded(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Text(
+                      _selectedDate == null
+                          ? 'No date selected'
+                          : formatter.format(_selectedDate!),
+                    ),
+                    IconButton(
+                      onPressed: _presentDatePicker,
+                      icon: const Icon(Icons.calendar_month),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+          const SizedBox(height: 16,),
+          Row(
+            children: [
+              DropdownButton(
+                value: _selectedCategory,
+                items: Category.values
+                    .map(
+                      (category) => DropdownMenuItem(
+                        value: category,
+                        child: Text(
+                          category.name.toUpperCase(),
+                        ),
+                      ),
+                    ).toList(),
+                onChanged: (value) {
+                  if (value == null) return;
+                  setState(() {
+                    _selectedCategory = value;
+                  });
+                },
+              ),
+              const Spacer(),
+              TextButton(
+                onPressed: () {
+                  print(_titleController.text);
+                  Navigator.pop(context);
+                },
+                child: const Text(
+                  'Cancel',
+                ),
+              ),
+              ElevatedButton(
+                onPressed: _submitExpenseData,
+                child: Text('Save Expanse'),
+              ),
+            ],
           ),
         ],
       ),
     );
   }
-
-  
 }
